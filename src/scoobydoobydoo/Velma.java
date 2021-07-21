f20fef393607f29b10d72a46dabec23f0686c92b
hcastro <hcastro98@gmail.com>
2021-07-21 18:07:28 +0100

    Added some file to .gitignore

--- src/scoobydoobydoo/Velma.java
+++ src/scoobydoobydoo/Velma.java
@@ -1,3 +1,18 @@
 package scoobydoobydoo;
 
-public class Velma{}
+public class Velma{
+    public Velma() {
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @java.lang.Override
+
+    public java.lang.String toString()
+    {
+        return "Velma{}";
+    }
+}
